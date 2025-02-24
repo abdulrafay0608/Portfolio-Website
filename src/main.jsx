6219d50688623f0b1c77bd3ea36c48fcba7a1603
Abdul Rafay <121513581+abdulrafay0608@users.noreply.github.com>
2025-02-25 02:59:55 +0500

    Fixed Import Error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Layout from "./components/layout/Layout.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
